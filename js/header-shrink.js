f4f92907a39dcc40f41b7f66d9086c491ce4ac7b
liuchenbaidu <563818835@qq.com>
2024-05-04 22:04:50 +0800

    Site updated: 2024-05-04 22:04:50

--- js/header-shrink.js
+++ js/header-shrink.js
@@ -1,41 +1,74 @@
-const pageTemplateDom = document.querySelector('.page-main-content');
-const articleTocContainerDom = document.querySelector('.article-toc-container');
-const headerDom = document.querySelector('.header-wrapper');
-const menuBarDom = document.querySelector('.menu-bar');
-const windowMaskDom = document.querySelector('.window-mask');
-const scrollPercentDom = document.querySelector('.scroll-percent');
+/* global KEEP */
 
-let isHeaderShrink = false;
-const headerHeight = headerDom.getBoundingClientRect().height;
+KEEP.initHeaderShrink = () => {
+  KEEP.utils.headerShrink = {
+    headerWrapperDom: null,
+    isHeaderShrink: false,
+    headerHeight: 70,
 
-window.addEventListener('scroll', function (_e) {
-    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-    if (!isHeaderShrink && scrollTop > headerHeight) {
-        isHeaderShrink = true;
-        headerDom.classList.add('header-wrapper-shrink');
-        pageTemplateDom.classList.add('page-main-content-top-shrink');
+    init() {
+      this.headerWrapperDom = document.querySelector('.header-wrapper')
+      if (this.headerWrapperDom) {
+        this.headerHeight = this.headerWrapperDom.getBoundingClientRect().height
+      }
+    },
 
-        if (articleTocContainerDom) {
-            articleTocContainerDom.classList.add('article-toc-container-shrink');
+    headerShrink() {
+      const fullPageHeight = Math.max(
+        document.body.scrollHeight,
+        document.documentElement.scrollHeight
+      )
+      if (fullPageHeight < window.innerHeight + 2 * this.headerHeight) {
+        return
+      }
+
+      const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+      const isHeaderTransparent =
+        KEEP.theme_config?.first_screen?.enable === true &&
+        !window.location.pathname.includes('/page/')
+
+      if (!this.isHeaderShrink && scrollTop > this.headerHeight) {
+        this.isHeaderShrink = true
+        document.body.classList.add('header-shrink')
+        if (isHeaderTransparent) {
+          this.headerWrapperDom.classList.add('transparent-2')
         }
-    } else if (isHeaderShrink && scrollTop <= headerHeight) {
-        isHeaderShrink = false;
-        headerDom.classList.remove('header-wrapper-shrink');
-        pageTemplateDom.classList.remove('page-main-content-top-shrink');
-
-        if (articleTocContainerDom) {
-            articleTocContainerDom.classList.remove('article-toc-container-shrink');
+      } else if (this.isHeaderShrink && scrollTop <= this.headerHeight) {
+        this.isHeaderShrink = false
+        document.body.classList.remove('header-shrink')
+        if (isHeaderTransparent) {
+          this.headerWrapperDom.classList.remove('transparent-2')
         }
+      }
+    },
+
+    sideToolsBarShowHandle() {
+      const scrollTop = document.body.scrollTop || document.documentElement.scrollTop
+      const sideToolsDom = document.querySelector('.side-tools .side-tools-container')
+      if (scrollTop > this.headerHeight / 2) {
+        sideToolsDom.classList.add('show')
+      } else {
+        sideToolsDom.classList.remove('show')
+      }
+    },
+
+    toggleHeaderDrawerShow() {
+      const domList = [document.querySelector('.window-mask'), document.querySelector('.menu-bar')]
+
+      if (KEEP.theme_config?.pjax?.enable === true) {
+        domList.push(
+          ...document.querySelectorAll('.header-drawer .drawer-menu-list .drawer-menu-item')
+        )
+      }
+
+      domList.forEach((v) => {
+        v.addEventListener('click', () => {
+          document.body.classList.toggle('show-header-drawer')
+        })
+      })
     }
-});
-
-
-menuBarDom.addEventListener('click', function (_e) {
-    headerDom.classList.toggle('header-drawer-show');
-});
-
-windowMaskDom.addEventListener('click', function (_e) {
-    headerDom.classList.toggle('header-drawer-show');
-});
-
-
+  }
+  KEEP.utils.headerShrink.init()
+  KEEP.utils.headerShrink.headerShrink()
+  KEEP.utils.headerShrink.toggleHeaderDrawerShow()
+}
--- js/main.js
+++ js/main.js
@@ -1,47 +1,82 @@
+/* global KEEP */
+
 window.addEventListener('DOMContentLoaded', () => {
-  console.log(`${CONFIG.themeInfo.name} v${CONFIG.themeInfo.version}`);
+  const { version, local_search, lazyload } = KEEP.theme_config
 
-  pageAsideOpenToggle();
-
-});
-
-
-const pageAsideOpenToggle = () => {
-
-  const toggleDom = document.querySelector('.page-aside-toggle');
-  const pageTopDom = document.querySelector('.page-main-content-top');
-  const containerDom = document.querySelector('.page-container');
-  const leftAsideDom = document.querySelector('.page-aside');
-
-  const headerContentDom = document.querySelector('.header-wrapper .header-content');
-  const mainContentDom = document.querySelector('.page-main-content-middle .main-content');
-
-  let isOpen = false;
-  const openToggle = (isOpen) => {
-
-    const pageAsideWidth = '248px';
-    containerDom.style.paddingLeft = isOpen ? pageAsideWidth : '0';
-    pageTopDom.style.paddingLeft = isOpen ? pageAsideWidth : '0';
-    pageTopDom.style.paddingLeft = isOpen ? pageAsideWidth : '0';
-    leftAsideDom.style.left = isOpen ? '0' : `-${pageAsideWidth}`;
-
-    headerContentDom.style.width = isOpen ? '76%' : '62%';
-    mainContentDom.style.width = isOpen ? '76%' : '62%';
-
-  }
-
-
-  toggleDom.addEventListener('click', () => {
-    isOpen = !isOpen;
-    openToggle(isOpen);
-  })
-
-
-  window.utils = {
-    openToggle: () => {
-      isOpen = true;
-      openToggle(isOpen);
+  KEEP.themeInfo = {
+    theme: `Keep v${version}`,
+    author: 'XPoet',
+    repository: 'https://github.com/XPoet/hexo-theme-keep',
+    localStorageKey: 'KEEP-THEME-STATUS',
+    styleStatus: {
+      isDark: false,
+      fontSizeLevel: 0,
+      isShowToc: true
     }
   }
 
-}
+  // print theme base info
+  KEEP.printThemeInfo = () => {
+    console.log(
+      `\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`,
+      `color: #fadfa3; background: #333; padding: 6px 0;`,
+      `padding: 6px 0;`
+    )
+  }
+  KEEP.printThemeInfo()
+
+  // set version number of footer
+  KEEP.setFooterVersion = () => {
+    const vd = document.querySelector('.footer .keep-version')
+    vd && (vd.innerHTML = KEEP.themeInfo.theme)
+    const vd2 = document.querySelector('.footer .shields-keep-version')
+    vd2 && (vd2.src = vd2.src.replace('Keep', KEEP.themeInfo.theme))
+  }
+
+  // set styleStatus to localStorage
+  KEEP.setStyleStatus = () => {
+    localStorage.setItem(KEEP.themeInfo.localStorageKey, JSON.stringify(KEEP.themeInfo.styleStatus))
+  }
+
+  // get styleStatus from localStorage
+  KEEP.getStyleStatus = () => {
+    let temp = localStorage.getItem(KEEP.themeInfo.localStorageKey)
+    if (temp) {
+      temp = JSON.parse(temp)
+      for (let key in KEEP.themeInfo.styleStatus) {
+        KEEP.themeInfo.styleStatus[key] = temp[key]
+      }
+      return temp
+    } else {
+      return null
+    }
+  }
+
+  // init prototype function
+  KEEP.initPrototype = () => {
+    HTMLElement.prototype.wrap = function (wrapper) {
+      this.parentNode.insertBefore(wrapper, this)
+      this.parentNode.removeChild(this)
+      wrapper.appendChild(this)
+    }
+  }
+  KEEP.initPrototype()
+
+  KEEP.initExecute = () => {
+    KEEP.initUtils()
+    KEEP.initHeaderShrink()
+    KEEP.initModeToggle()
+    KEEP.initBack2Top()
+    KEEP.initCodeBlock()
+    KEEP.setFooterVersion()
+
+    if (local_search?.enable === true) {
+      KEEP.initLocalSearch()
+    }
+
+    if (lazyload?.enable === true) {
+      KEEP.initLazyLoad()
+    }
+  }
+  KEEP.initExecute()
+})
